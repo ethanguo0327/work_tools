7a117f3888ae9970bf38b700129266577a6c8763
ethankwok <guoyangquan@gs-robot.com>
2019-11-07 20:04:37 +0800

    keep scan_nav

--- gmapping_filter.bash
+++ gmapping_filter.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 name=$1
-rosbag filter $name ${name}_filtered.bag 'topic == "/device/operational_device_status" or topic == "/gps" or topic == "/odom" or topic == "/device/rfid" or topic == "/device/gyro" or topic == "/scan" or topic == "/rgb_image_hd" or topic == "/scan_2" or topic == "/initialpose_moving" or topic == "/initialpose" or topic == "/initialpose_current" or (topic == "/tf" and m.transforms[0].header.frame_id != "world")'
+rosbag filter $name ${name}_filtered.bag 'topic == "/device/operational_device_status" or topic == "/gps" or topic == "/odom" or topic == "/device/rfid" or topic == "/device/gyro" or topic == "/scan_nav" or topic == "/rgb_image_hd" or topic == "/scan_2" or topic == "/initialpose_moving" or topic == "/initialpose" or topic == "/initialpose_current" or (topic == "/tf" and m.transforms[0].header.frame_id != "world")'
